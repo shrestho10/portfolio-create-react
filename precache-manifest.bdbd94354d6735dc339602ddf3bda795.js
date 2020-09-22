d4ebe769a1cb62b74563d1408e7082ace475fe54
shrestho10 <shresthokucse17@gmail.com>
2020-09-22 22:20:16 +0600

    Updates

---
+++ precache-manifest.bdbd94354d6735dc339602ddf3bda795.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7dc4cdd82f7a7684458a4561f40154",
+    "url": "/shrestho10/portfolio-create-react/index.html"
+  },
+  {
+    "revision": "910a8b1b98178b74b16b",
+    "url": "/shrestho10/portfolio-create-react/static/css/main.8dd624e5.chunk.css"
+  },
+  {
+    "revision": "5e7e8322f993ade69bac",
+    "url": "/shrestho10/portfolio-create-react/static/js/2.241f38e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shrestho10/portfolio-create-react/static/js/2.241f38e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "910a8b1b98178b74b16b",
+    "url": "/shrestho10/portfolio-create-react/static/js/main.bde3b02a.chunk.js"
+  },
+  {
+    "revision": "150310b8bf4226367bf7",
+    "url": "/shrestho10/portfolio-create-react/static/js/runtime-main.b161d081.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shrestho10/portfolio-create-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6f9dccbc95cd81a7a9ca2fadf471c768",
+    "url": "/shrestho10/portfolio-create-react/static/media/myimg.6f9dccbc.png"
+  }
+]);
